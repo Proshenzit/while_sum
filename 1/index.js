20eecc1ab3e7f804842f5c678927e462ee6da699
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-16 00:21:20 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,42 @@
+var x = 2;
+
+var sum=0;
+
+
+while (x <=50){
+
+    if(x%3==0 && x%5==0)
+    {
+
+        document.write("  " +x);
+        sum=sum+x;
+
+    }
+
+    
+    x = x + 1;  
+}
+
+document.write(" sum =  "+sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
